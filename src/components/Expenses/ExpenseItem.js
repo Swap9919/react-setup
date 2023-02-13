85b532ae4e42f29009b7f5a25629641ac4860c20
Sai Swaroop <saiswaroop874@gmail.com>
2023-02-13 09:56:14 +0300

    c

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css"
-import Card from "./Card";
+import Card from "../UI/Card";
 
 function ExpenseItem(props) {
 
