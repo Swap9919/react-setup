1fc9031d223574c0b2bdefa61fb052470f8b5e06
Sai Swaroop <saiswaroop874@gmail.com>
2023-03-11 14:59:35 +0530

    c

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,7 +3,7 @@ import React from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
-
+  
 const ExpenseItem = (props) => {
       return (
             <Card className='expense-item'>
